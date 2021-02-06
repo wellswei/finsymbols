2f512d14a7b39187ab9c01bcacfc77237099240f
wellswei <wellswei88@gmail.com>
2021-02-06 14:12:47 -0500

    add russell 1000 index from wiki

--- finsymbols/symbols.py
+++ finsymbols/symbols.py
@@ -3,6 +3,30 @@ import sys
 from bs4 import BeautifulSoup
 from finsymbols.symbol_helper import *
 
+def get_russell_1000_symbols():
+    page_html = wiki_html('Russell_1000_Index', 'Russell_1000.html')
+    wiki_soup = BeautifulSoup(page_html, "html.parser")
+    symbol_table = wiki_soup.find(attrs={'class': 'wikitable sortable'})
+
+    symbol_data_list = list()
+
+    for symbol in symbol_table.find_all("tr"):
+        symbol_data_content = dict()
+        symbol_raw_data = symbol.find_all("td")
+        td_count = 0
+        for symbol_data in symbol_raw_data:
+            if(td_count == 0):
+                symbol_data_content[
+                    'company'] = symbol_data.text
+            elif(td_count == 1):
+                symbol_data_content[
+                    'symbol'] = symbol_data.text[:-2]
+
+            td_count += 1
+
+        symbol_data_list.append(symbol_data_content)
+
+    return symbol_data_list[1::]
 
 def get_sp500_symbols():
     page_html = wiki_html('List_of_S%26P_500_companies', 'SP500.html')
