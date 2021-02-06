18c58d105973541238d004dd40463d330f15cbf3
wellswei <wellswei88@gmail.com>
2021-02-06 14:13:28 -0500

    added russell 1000 symbols

--- finsymbols/__init__.py
+++ finsymbols/__init__.py
@@ -2,3 +2,4 @@ from finsymbols.symbols import get_sp500_symbols
 from finsymbols.symbols import get_nyse_symbols
 from finsymbols.symbols import get_amex_symbols
 from finsymbols.symbols import get_nasdaq_symbols
+from finsymbols.symbols import get_russell_1000_symbols
